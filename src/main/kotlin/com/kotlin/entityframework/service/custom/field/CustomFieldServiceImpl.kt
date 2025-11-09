0b40310c82afeca832938baca3b054ac0f75c719
Бачище Назар Сергеевич <mcarim@mail.ru>
2025-11-09 23:18:31 +0300

    .

--- src/main/kotlin/com/kotlin/entityframework/service/custom/field/CustomFieldServiceImpl.kt
+++ src/main/kotlin/com/kotlin/entityframework/service/custom/field/CustomFieldServiceImpl.kt
@@ -14,6 +14,7 @@ class CustomFieldServiceImpl(
 ) : CustomFieldService {
     override fun createCustomFieldsToEntityType(customFieldsRequest: CustomFieldRequest): List<CustomField> {
         TODO("Not yet implemented")
+
     }
 
     @Transactional
